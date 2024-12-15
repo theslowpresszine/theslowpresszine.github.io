f6dddccd1ef35cb687a314c46c130b5c6f4b3668
Amalina <125463377+amalinalai@users.noreply.github.com>
2024-12-15 13:31:59 +0800

    Close modal on submit

--- scripts/newsletter-modal.js
+++ scripts/newsletter-modal.js
@@ -21,3 +21,15 @@ function forgetMe() {
   localStorage.dontShow = "true";
   modal.style.display = "none";
 }
+
+function submitFormModal() {
+   // Get the first form with the name
+   // Usually the form name is not repeated
+   // but duplicate names are possible in HTML
+   // Therefore to work around the issue, enforce the correct index
+   var frm = document.getElementsByName('modal-form')[0];
+   frm.submit(); // Submit the form
+   frm.reset();  // Reset all form data
+   forgetMe();  // never prompt modal again
+   return false; // Prevent page refresh
+}
