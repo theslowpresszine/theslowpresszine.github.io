76121c2d13248e767a5bcf182337ed53b5099dad
Amalina <125463377+amalinalai@users.noreply.github.com>
2026-03-13 14:55:09 +0800

    Shift newsletter script

--- scripts/html-include.js
+++ scripts/html-include.js
@@ -25,3 +25,21 @@ function includeHTML() {
 		}
 	  }
 	};
+	
+function submitFormBottom() {
+	// Get the first form with the name
+	// Usually the form name is not repeated
+	// but duplicate names are possible in HTML
+	// Therefore to work around the issue, enforce the correct index
+	var frm = document.getElementsByName('bottom-form')[0];
+    let x = document.forms["bottom-form"]["validation"].value;
+	if (x == "4") {
+		frm.submit(); // Submit the form
+		frm.reset();  // Reset all form data
+		return false; // Prevent page refresh
+	} else {
+		alert("Failed validation.");
+		frm.reset();
+		return false;
+	}
+};
